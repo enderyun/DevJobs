3232b5b78bc984a040196f0626ecedb33a5d11ca
enderyun <enderyum1524@gmail.com>
2026-01-07 16:26:41 -0500

    Link.jsx dinamico y template de Contact.jsx

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,17 +1,27 @@
 import { useRouter } from "../hooks/useRouter.jsx";
 
-export function Link({ href, children, ...restOfProps }) {
+export function Link({ href, children, className = "", ...restOfProps }) {
+  const { navigateTo, currentPath } = useRouter();
 
-  const { navigateTo } = useRouter()
+  // href: dirrecion desde el Route.jsx/App.jsx
+  // currentPath: window.location.pathname del useRouter
 
   const handleClick = (event) => {
     event.preventDefault();
-    
-    navigateTo(href)
+    navigateTo(href);
   };
 
+  const linkIsActive = currentPath === href ? "isActive" : "";
+
   return (
-    <a href={href} {...restOfProps} onClick={handleClick}>
+    <a
+      href={href}
+      onClick={handleClick}
+      // Por lo general no se deberia de pasar ningun atributo adicional a
+      // este componente, pero si se cambia de parecer en el futuro no hay problema
+      className={`${className} ${linkIsActive}`}
+      {...restOfProps}
+    >
       {children}
     </a>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,30 @@
+/*
+  Este es un jsx que solo sirve como practica. Probablemente se cambien muchas de las cosas
+  que se veran a continuacion en el futuro. 
+*/
+
 export function ContactPage() {
+
+  // Logica temporal aca
+
   return (
     <div>
       <h1>Contacto</h1>
       <p>¿Tienes alguna pregunta? Contáctanos.</p>
+
+      <form>
+        <label htmlFor="name">Nombre</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="number">Numero de telefono</label>
+        <input type="number" id="number" name="number" />
+
+        <button type="submit">Enviar</button>
+
+      </form>
     </div>
   )
 }
