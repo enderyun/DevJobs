7fa05711d574ac5f8a2f1b48304e7fade0a6c0b8
enderyun <enderyum1524@gmail.com>
2026-02-03 18:30:03 -0500

    Stagehand en el proyecto y .gitignore con los .env
    He eliminado el archivo express/.env del repositorio y agregado los .env al .gitignore
    para dar paso a las variables de entorno secretas. Aunque ya habia un archivo con variable
    de entorno, no era importante ya que solo tenia un puerto para levantar la API.
    
    Adicional a eso, he agregado Stagehand para realizar tests e2e con IA para tenerlo en conjunto
    con Playwright.

---
+++ testing/ai-e2e-test/test-ai.js
@@ -0,0 +1,11 @@
+import { test } from "node:test";
+import assert from "node:assert";
+
+import { Stagehand } from "@browserbasehq/stagehand";
+
+test('Un usuario puede entrar a la jsconf y adquirir 2 entradas por 287.98 euros', async () => {
+  const stagehand = new Stagehand({
+    env: 'LOCAL',
+    model: 
+  });
+})
