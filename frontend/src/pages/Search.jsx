9663462091296a1c5d0811606394b1607c137885
enderyun <enderyum1524@gmail.com>
2026-03-02 22:56:28 -0500

    Integracion de Jest al proyecto
    Agregado como experimentacion. Aunque Jest es un buen framework para realizar pruebas unitarias,
    en este proyecto la mejor opcion es Vitest. Poseen una sintaxis similar, asi que aprender uno es
    mas o menos equivalente en aprender el otro.

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -57,7 +57,6 @@ const useFilters = () => {
 
         const queryParams = params.toString()
 
-        // const response = await fetch(`https://jscamp-api.vercel.app/api/jobs?${queryParams}`)
         const response = await fetch(`http://localhost:3000/jobs?${queryParams}`)
         const json = await response.json()
 
